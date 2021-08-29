21e15d8e4eb243524698b5355f8e070ecf61f5d7
A-Dolah <adam@insurely.se>
2021-08-29 14:28:48 +0200

    fix

--- components/containers/Navbar/index.tsx
+++ components/containers/Navbar/index.tsx
@@ -7,21 +7,19 @@ const NavbarRoot: FC = () => {
   const [hasScrolled, setHasScrolled] = useState(false)
 
   useEffect(() => {
-  //   const handleScroll = throttle(() => {
-  //     const offset = 0
-  //     const { scrollTop } = document.documentElement
-  //     const scrolled = scrollTop > offset
-
-  //     if (hasScrolled !== scrolled) {
-  //       setHasScrolled(scrolled)
-  //     }
-  //   }, 200)
-
-  //   document.addEventListener(`scroll`, handleScroll)
-  //   return () => {
-  //     document.removeEventListener(`scroll`, handleScroll)
-  //   }
-  // }, [hasScrolled])
+    //   const handleScroll = throttle(() => {
+    //     const offset = 0
+    //     const { scrollTop } = document.documentElement
+    //     const scrolled = scrollTop > offset
+    //     if (hasScrolled !== scrolled) {
+    //       setHasScrolled(scrolled)
+    //     }
+    //   }, 200)
+    //   document.addEventListener(`scroll`, handleScroll)
+    //   return () => {
+    //     document.removeEventListener(`scroll`, handleScroll)
+    //   }
+  }, [hasScrolled])
 
   return (
     <div>
