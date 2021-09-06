e01e9b4ede785bd85cab3619a0f0f9f80f875ab9
A-Dolah <adam@insurely.se>
2021-09-06 18:31:44 +0200

    Main content fixes

--- components/ui-components/LandingInitialInfo/index.jsx
+++ components/ui-components/LandingInitialInfo/index.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import Image from 'next/image'
+
+import dalbyGastisPic from '@public/gastis_sky_view.jpeg'
 
 const LandingInitialInfo = () => (
-  <section className="flex-row items-center text-center	gap-3 h-4/6">
-    <h1 className="text-4xl">Annika och Adams bröllopssida</h1>
-
+  <section className="flex flex-col items-center text-center	gap-3 h-4/6">
+    {/* <h1 className="text-6xl my-4">Annika och Adams bröllopssida</h1> */}
+    <div className="w-10/12">
+      <Image src={dalbyGastisPic} alt="Gästis sky view" priority placeholder="blur" />
+    </div>
     <div>Info</div>
-    <p className="text-base">Anmäl dig här:</p>
   </section>
 )
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,9 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center">
-      <main>
-        <LandingInitialInfo />
-        <AttendForm />
-      </main>
+    <div className="flex flex-col items-center content-center">
+      <LandingInitialInfo />
+      <AttendForm />
     </div>
   )
 }
