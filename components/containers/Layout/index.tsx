055adbd978966e302655ab18859de70e9faded21
A-Dolah <adam@insurely.se>
2021-09-06 18:30:50 +0200

    Art deco style layout

--- components/containers/Layout/index.tsx
+++ components/containers/Layout/index.tsx
@@ -1,11 +1,14 @@
+import cn from 'classnames'
 import { NextPage } from 'next'
 
 import { useRouter } from 'next/router'
 
 import React, { FC } from 'react'
 
-import Footer from '@containers/Footer'
+import Footer from '@components/Footer'
 import Navbar from '@containers/Navbar'
+
+import styles from './layout.module.css'
 
 interface Props {
   pageProps: {
@@ -17,10 +20,21 @@ const Layout: FC<Props> = ({ children }) => {
   const { locale = `sv-SE` } = useRouter()
 
   return (
-    <div className="flex flex-col">
-      <Navbar />
-      <main className="">{children}</main>
-      <Footer />
+    <div className="p-2">
+      <div className={cn(styles.squareDecoContainer, styles.squareDecoContainer)}>
+        <div className={`flex flex-col ${styles.squareDecoContent}`}>
+          <Navbar />
+          <main className="p-6 sm:p-11">{children}</main>
+          <Footer />
+        </div>
+        <div className={styles.squareDecoInner} />
+        <div className={styles.squareDecoSquareLeftTop} />
+        <div className={styles.squareDecoSquareLeftBottom} />
+        <div className={styles.squareDecoSquareRightTop} />
+        <div className={styles.squareDecoSquareRightBottom} />
+        <div className={styles.squareDecoTall} />
+        <div className={styles.squareDecoWide} />
+      </div>
     </div>
   )
 }
