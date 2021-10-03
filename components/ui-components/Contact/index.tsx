696bac5a66bec2ac1f8b1f884147a6cdc0a37083
A D <ad@As-MacBook-Pro.local>
2021-10-03 20:44:58 +0200

    Contact: add shell for component

---
+++ components/ui-components/Contact/index.tsx
@@ -0,0 +1,19 @@
+import BorderFrame from '@components/BorderFrame'
+
+const Contact = () => (
+  <div className="bg-white w-full sm:w-10/12 xl:w-1/2 p-4 flex flex-col items-center">
+    <p className="font-sans mt-12 mb-8 text-base xl:text-lg font-thin text-center">
+      Om ni har några frågor, tveka inte att höra av er till oss eller våra toastmasters!.
+    </p>
+    <p className="mb-2 text-2xl xl:text-3xl font-thin ">OSA</p>
+
+    <BorderFrame>
+      <div className="flex items-center justify-center justify-between mt-4">
+        <div className="text-gray-900 relative uppercase font-thin text-lg sm:text-xl tracking-widest mx-2 whitespace-nowrap text-center">
+          Kontaktuppgifter till brudparet:
+        </div>
+      </div>
+    </BorderFrame>
+  </div>
+)
+export default Contact
