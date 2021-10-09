fe67ebebab129f687f914f1c3e6d934ed7d7f05a
a-dolah <a.dolah.dev@gmail.com>
2021-10-09 12:40:57 +0200

    Finish schedule page

--- components/ui-components/Schedule/index.tsx
+++ components/ui-components/Schedule/index.tsx
@@ -44,7 +44,7 @@ const ScheduleItem: FC<ItemProps> = ({ time, happening, description, icon }) => 
   return (
     <div className="flex flex-col md:flex-row items-center text-center md:text-left md:items-start justify-between gap-4 md:gap-8 my-8 md:my-4 pb-4 sm:pb-0 border-b sm:border-none">
       <span className="text-base font-extralight w-32 whitespace-nowrap">{time}</span>
-      {(width as unknown as number) < 768 && (
+      {(width as unknown as number) < 800 && (
         <div className="w-auto sm:w-32 self-center flex justify-center">{icon}</div>
       )}
       <div className="md:w-64">
@@ -53,7 +53,7 @@ const ScheduleItem: FC<ItemProps> = ({ time, happening, description, icon }) => 
           <span className="text-sm italic font-extralight">{description}</span>
         </div>
       </div>
-      {(width as unknown as number) >= 768 && (
+      {(width as unknown as number) >= 800 && (
         <div className="w-auto sm:w-32 self-center flex justify-center">{icon}</div>
       )}
     </div>
@@ -66,7 +66,7 @@ const Schedule = () => (
       Till bröllopet samlas Vi vänner och familj från många olika delar av Sverige och världen. Vi
       passar därför på att umgås och fira hela helgen. Här finner Ni schemat för helgen!
     </p>
-    <p className="mb-6 text-2xl xl:text-3xl font-extralight">Schema Annika & Adams Bröllopshelg</p>
+    <p className="mb-6 text-2xl xl:text-3xl font-extralight">Schema för bröllopshelgen</p>
 
     <BorderFrame>
       <div className="flex items-center justify-center sm:justify-between mt-4">
@@ -75,7 +75,7 @@ const Schedule = () => (
       <ScheduleItem
         time="16.00 – 23.00"
         happening="Grillfest, Tetravägen 74 Dalby"
-        description="Det bjuds in till grillfest i Adams föräldrars trädgård. Här bjuds det på mat och dryck varvat med aktiviteter. Kom när det passar!"
+        description="Det bjuds in till grillfest i Adams föräldrars trädgård. Här bjuds det på mat och dryck. Vi anordnar också lite olika aktiviteter. Kom när det passar!"
         icon={<BarbecueIcon width={42} height={42} />}
       />
 
@@ -85,19 +85,19 @@ const Schedule = () => (
       <ScheduleItem
         time="14.00 – 15.30"
         happening="Vigsel, Dalby (eller Torna Hällestad) Kyrka"
-        description="Vigsel är planerad att ske i Dalby kyrka, ett stenkast från festlokalen Dalby Gästis Tingshus. Dalby Kyrka kan dock komma att genomgå renovering, och då sker vigsel istället i grannbyn Torna Hällestads kyrka."
+        description="Vigsel är planerad till Dalby kyrka, ett stenkast från festlokalen Dalby Gästis Tingshus. Dalby kyrka kan dock komma att genomgå renovering, och då sker vigsel istället i grannbyn Torna Hällestads kyrka."
         icon={<ChurchIcon width={42} height={42} />}
       />
       <ScheduleItem
         time="16.00 – 17.00"
         happening="Fördrink, Dalby Gästis trädgård"
-        description="Efter vigsel och innan bröllopsmiddagen samlas vi i Dalby Gästis härliga trädgård för fördrink, snittar, mingel och bilder."
+        description="Efter vigsel och innan bröllopsmiddagen samlas vi i Dalby Gästis härliga trädgård för bubbel, snittar, mingel och bilder."
         icon={<MingleIcon width={42} height={42} />}
       />
       <ScheduleItem
         time="17.00 – 21.00"
         happening="Middag, Dalby Gästis Tingshus"
-        description="I Dalby Gästis nyrenoverade Tingshus har vi bröllopsmiddag som anordas av krögarna på Linnérs mat. Här finns möjlighet att hålla tal och uppträda. Det är Annikas bror Per och Adams syster Sarah håller i middagen, så stäm gärna av tal- och uppträdanden med dem!"
+        description="I Dalby Gästis nyrenoverade Tingshus har vi bröllopsmiddag som anordas av krögarna som driver Dalby Gästis. Här finns möjlighet att hålla tal och/eller uppträda. Det är Annikas bror Per och Adams syster Sarah håller i middagen, så stäm gärna av tal- och uppträdanden med dem!"
         icon={<DiningIcon width={42} height={42} />}
       />
       <ScheduleItem
@@ -112,8 +112,8 @@ const Schedule = () => (
       </div>
       <ScheduleItem
         time="10.00 – 13.00"
-        happening="Brunch, Dalby Gästis Restaurang"
-        description="Här kan man komma förbi och säga hejdå."
+        happening="Brunch, Dalby Gästis Hotell"
+        description='Detta är inte en "officiell" del av bröllopshelgen, men Vi (Annika och Adam) kommer att befinna oss på Dalby Gästis under söndagen för att ta farväl och tacka för helgen. Kom om Ni har vägarna förbi innan Er hemresa!'
         icon={<TeaIcon width={42} height={42} />}
       />
     </BorderFrame>
