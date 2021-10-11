fe0ad919f670ca22c5796cdad16c5a559e6b8e6e
a-dolah <a.dolah.dev@gmail.com>
2021-10-11 21:33:31 +0200

    finished website

--- components/ui-components/FAQ/index.tsx
+++ components/ui-components/FAQ/index.tsx
@@ -2,11 +2,36 @@ import BorderFrame from '@components/BorderFrame'
 
 const FAQ = () => (
   <div className="bg-white w-full sm:w-10/12 xl:w-1/2 p-2 flex flex-col items-center">
-    <p className="font-sans mt-6 mb-10 text-base xl:text-lg font-extralight text-center">
+    <p className="font-sans mt-6 text-base xl:text-lg font-extralight text-center">
       Här samlar vi matnyttig information om bröllopshelgen. Denna sida kommer att uppdateras
       löpande.
     </p>
-    <p className="mb-4 text-2xl xl:text-3xl font-extralight">Boende</p>
+
+    <p className="mb-4 text-2xl xl:text-3xl font-extralight mt-8">Klädsel</p>
+    <BorderFrame>
+      <div className="flex-col justify-center items-center text-center">
+        <div className="italic font-extralight text-center">
+          Till bröllopet har vi valt klädkoden "mörk kostym". Det innebär i grova drag att om man
+          vill ha på sig kostym brukar den ha en mörk färg (t.ex. mörkblå, mörkbrun eller mörkgrå).
+          Skorna ska traditionellt vara svarta, men detta har Adam brutit mot flera gånger. Slips
+          eller fluga passar fint till mörk kostym! Bär man hellre klänning eller kjol brukar den
+          vara av lite finare tyg och täcka knäna.
+        </div>
+      </div>
+    </BorderFrame>
+
+    <p className="mb-4 text-2xl xl:text-3xl font-extralight mt-8">Barn</p>
+    <BorderFrame>
+      <div className="flex-col justify-center items-center text-center">
+        <div className="italic font-extralight text-center">
+          Barn är varmt välkomna till grillfesten på fredagen den 27 maj. På lördagen den 28 maj
+          tror vi däremot att barnen har det roligare hos sina barnvakter. <br />
+          Undantag: Ammande barn är välkomna hela helgen.
+        </div>
+      </div>
+    </BorderFrame>
+
+    <p className="mb-4 text-2xl xl:text-3xl font-extralight mt-8">Boende</p>
     <BorderFrame>
       <div className="flex-col justify-center items-center text-center">
         <div className="italic font-extralight text-center">Mer info kommer inom kort</div>
@@ -14,13 +39,6 @@ const FAQ = () => (
     </BorderFrame>
 
     <p className="mb-4 text-2xl xl:text-3xl font-extralight mt-8">Resa</p>
-    <BorderFrame>
-      <div className="flex-col justify-center items-center text-center">
-        <div className="italic font-extralight text-center">Mer info kommer inom kort</div>
-      </div>
-    </BorderFrame>
-
-    <p className="mb-4 text-2xl xl:text-3xl font-extralight mt-8">Klädsel</p>
     <BorderFrame>
       <div className="flex-col justify-center items-center text-center">
         <div className="italic font-extralight text-center">Mer info kommer inom kort</div>
