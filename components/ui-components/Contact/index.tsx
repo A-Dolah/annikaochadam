9cbd55901580ec4bbdde63b7495f8480923094f4
a-dolah <a.dolah.dev@gmail.com>
2021-10-11 19:14:29 +0200

    Contact fix

--- components/ui-components/Contact/index.tsx
+++ components/ui-components/Contact/index.tsx
@@ -1,7 +1,7 @@
 import BorderFrame from '@components/BorderFrame'
 
 const Contact = () => (
-  <div className="bg-white w-full sm:w-10/12 xl:w-1/2 p-4 flex flex-col items-center">
+  <div className="bg-white w-full sm:w-10/12 xl:w-1/2 p-2 flex flex-col items-center">
     <p className="font-sans mt-6 mb-10 text-base xl:text-lg font-extralight">
       Om ni har några frågor, tveka inte att höra av er till oss eller våra toastmasters!.
     </p>
@@ -9,8 +9,8 @@ const Contact = () => (
 
     <BorderFrame>
       <div className="flex-col justify-center items-center mt-4 text-center">
-        <div className="text-gray-900 relative uppercase font-extralight text-lg sm:text-xl tracking-widest mx-2 whitespace-nowrap">
-          Kontaktuppgifter till brudparet:
+        <div className="text-gray-900 relative uppercase font-extralight text-lg sm:text-xl tracking-widest mx-2">
+          Brudparet:
         </div>
         <div className="mt-4">
           <h3 className="mb-2">Email:</h3>
@@ -18,18 +18,23 @@ const Contact = () => (
             adamochannika@gmail.com
           </a>
         </div>
-        <div className="mt-4 mb-2">
+        <div className="mt-5 mb-5">
           <h3 className="mb-2">Telefon:</h3>
           <p className="font-extralight">Annika: 073-057 83 17</p>
           <p className="font-extralight">Adam: 0736-49 90 79</p>
         </div>
-        <div className="text-gray-900 mt-10 relative uppercase font-extralight text-lg sm:text-xl tracking-widest whitespace-nowrap">
-          Kontaktuppgifter till toastmasters:
+        <div className="border-b" />
+        <div className="text-gray-900 relative uppercase font-extralight text-lg sm:text-xl tracking-widest mt-5">
+          Toastmasters:
         </div>
-        <div className="mt-4">
+        <div className="mt-3">
           <h3 className="mb-2">Email:</h3>
           <div>
-            <span className="font-extralight">Sarah: </span>
+            <a className="font-extralight text-blue-500" href="mailto:per@gmail.com">
+              per@gmail.com
+            </a>
+          </div>
+          <div>
             <a
               className="font-extralight text-blue-500"
               href="mailto:sarah.nilsson.dolah@gmail.com"
@@ -37,17 +42,11 @@ const Contact = () => (
               sarah.nilsson.dolah@gmail.com
             </a>
           </div>
-          <div>
-            <span className="font-extralight">Per: </span>
-            <a className="font-extralight text-blue-500" href="mailto:per@gmail.com">
-              per@gmail.com
-            </a>
-          </div>
         </div>
         <div className="mt-4 mb-2">
           <h3 className="mb-2">Telefon:</h3>
-          <p className="font-extralight">Sarah: 073-875 79 00</p>
           <p className="font-extralight">Per: 070-544 84 96</p>
+          <p className="font-extralight">Sarah: 073-875 79 00</p>
         </div>
       </div>
     </BorderFrame>
