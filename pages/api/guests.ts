19564f17ab0f06f69734fea7a65632d086956aac
a-dolah <a.dolah.dev@gmail.com>
2021-10-11 21:34:54 +0200

    Dont list all guests

--- pages/api/guests.ts
+++ pages/api/guests.ts
@@ -10,14 +10,14 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     setupPgPool()
   }
 
-  if (req.method === `GET`) {
-    try {
-      const { rows } = await pgPool.query(selectAllGuestsQuery)
-      res.status(200).json({ guests: rows })
-    } catch (e) {
-      res.status(500).json({ error: e })
-    }
-  }
+  // if (req.method === `GET`) {
+  //   try {
+  //     const { rows } = await pgPool.query(selectAllGuestsQuery)
+  //     res.status(200).json({ guests: rows })
+  //   } catch (e) {
+  //     res.status(500).json({ error: e })
+  //   }
+  // }
 
   if (req.method === `POST`) {
     const { email, firstName, lastName, attending27, attending28, notAttending, diet } = req.body
